f0136999b1fd8344702c506f6d03d20f6c97641b
KatyaBadowiec <katya@badowiec.eu>
2023-01-25 10:50:59 +0100

    Task1

---
+++ Canddat/Candidates.cs
@@ -0,0 +1,20 @@
+﻿namespace Canddat
+{    
+    public class Candidates
+    {
+        public Person Person { get; set; }
+        public SubjectScore[] SubjectScores { get; set; }
+
+        public Candidates(Person person, SubjectScore[] scores) 
+        {
+            Person = person;
+            SubjectScores = scores;
+        }
+
+        public virtual string GetInfo()
+        {
+            return $"{Person} - {SubjectScores}";
+        }
+    }
+}
+
---
+++ Canddat/SubjectScore.cs
@@ -0,0 +1,14 @@
+﻿namespace Canddat
+{
+    public class SubjectScore
+    {
+        public string Subject { get; set; }
+        public int Score { get; set; }
+
+        public SubjectScore(string subject, int score)
+        {
+            Subject = subject;
+            Score = score;
+        }
+    }
+}
